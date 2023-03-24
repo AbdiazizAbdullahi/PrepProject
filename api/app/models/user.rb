d755ef7fcb5cb231af50c72e4be52d26a790c2fd
AbdiazizAbdullahi <taqiyudinabdullahi2001@gmail.com>
2023-03-24 05:06:46 +0300

    updated the api folder

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :todos
 
-    validates :username, {
-        length: { minimum: 5, maximum: 8 },
+    validates :username, { 
+        length: { minimum: 5, maximum: 8 }, 
         uniqueness: true,
         presence: true 
-    }
+    } 
 
     validates :email, {
-        uniqueness: true, 
+        uniqueness: true,
         presence: true
     }
 
